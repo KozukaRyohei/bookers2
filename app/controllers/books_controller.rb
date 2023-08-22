a0809647af3f1b3b21969b01e33bc1ce4453a794
KozukaRyohei <138641952+KozukaRyohei@users.noreply.github.com>
2023-08-22 08:54:14 +0000

    [Finish] bookのindexページの左側の作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,25 @@
 class BooksController < ApplicationController
+
+
+  def create
+    @user = User.new(post_image_params)
+    @user.user_id = current_user.id
+    if @post_image.save
+      redirect_to post_images_path
+    else
+      render :new
+    end
+  end
+
+  def index
+    @users = current_user
+    @user = User.new
+  end
+
+  private
+
+  def books_params
+    params.require(:books).permit(:name, :introduction)
+  end
+
 end
