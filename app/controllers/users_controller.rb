8ec0f7072126cede67d8e8a5814ee13947bcfd67
KozukaRyohei <138641952+KozukaRyohei@users.noreply.github.com>
2023-08-23 11:33:15 +0000

    [Finish] bookのeditページの作成 ～ bookのdestroy機能の作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,23 @@
 class UsersController < ApplicationController
-  
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :image, :introduction)
+  end
+
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to book_path
+    end
+  end
+
 end
