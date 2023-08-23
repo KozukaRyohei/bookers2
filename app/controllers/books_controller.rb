0de5392c74ee9e1f2b1a71488de61e8601add5e9
KozukaRyohei <138641952+KozukaRyohei@users.noreply.github.com>
2023-08-23 07:36:38 +0000

    [Finish]bookのshowページの作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,7 +5,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-      redirect_to book_path(@book.id)
+       redirect_to book_path(@book.id)
     else
        render :new #未変更
     end
@@ -15,6 +15,23 @@ class BooksController < ApplicationController
     @user = current_user
     @book = Book.new
     @books = Book.all
+  end
+
+  def show
+    @book_new = Book.new
+    @book =  Book.find(params[:id])#bookを作ったユーザーの情報を入れたい
+    @user = @book.user
+    #@userが作ったbookの内容(title,body)が欲しい
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
   private
